c796e35b79f00538fd12ca65d05244cb9e0e4dd0
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-11-03 22:49:26 +0700

    commit cuoi ngay

--- src/main/java/com/group3/exception/ExceptionHelper.java
+++ src/main/java/com/group3/exception/ExceptionHelper.java
@@ -16,4 +16,28 @@ public class ExceptionHelper {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<?> exception() {
+        return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<?> illegalArgument() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    public ResponseEntity<?> illegalState() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(IllegalAccessException.class)
+    public ResponseEntity<?> illegalAccess() {
+        return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<?> nullPointer() {
+        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
 }
--- src/main/java/com/group3/repositories/music/IMusicRepository.java
+++ src/main/java/com/group3/repositories/music/IMusicRepository.java
@@ -1,9 +1,19 @@
 package com.group3.repositories.music;
 
+import com.group3.models.category.Category;
 import com.group3.models.music.Music;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMusicRepository extends JpaRepository<Music, Long> {
+    Page<Music> findAllByNameContaining(String name);
+
+    Page<Music> findAllByCategory(Category category);
+
+    @Query(value = "SELECT * FROM music m WHERE m.category_id LIKE ?1 AND m.name LIKE ?2", nativeQuery = true)
+    Page<Music> findAllByNameWithCategory(@Param("1") Long category_id, @Param("2") String name);
 }
